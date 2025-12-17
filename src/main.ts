ce8ea56cc15637286bba183617cf331dadf28851
lonewolfyx <877085893@qq.com>
2025-12-17 14:20:37 +0800

    feat: add Swagger documentation support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { version } from '../package.json'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     app.enableCors()
+
+    const config = new DocumentBuilder().setTitle('Github Web Hooks').setDescription('').setVersion(version).build()
+    const documentFactory = () => SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, documentFactory, {
+        jsonDocumentUrl: 'api/json',
+        swaggerOptions: {
+            showExtensions: true
+        }
+    })
 
     await app.listen(process.env.PORT ?? 3000)
 
